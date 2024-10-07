07b2a5f68c0a43cbeb9ed1b4a78d42e8980c51f6
Zokni1Coder <erikkovacs0000@gmail.com>
2024-10-07 19:42:21 +0000

    init

---
+++ config/dev.secret.dist.exs
@@ -0,0 +1,16 @@
+import Config
+
+# Configure your database
+config :chat_app, ChatApp.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "chat_app_dev",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
+  config :chat_app, ChatAppWeb.Endpoint,
+  # Binding to loopback ipv4 address prevents access from other machines.
+  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+  http: [ip: {127, 0, 0, 1}, port: 4000]
