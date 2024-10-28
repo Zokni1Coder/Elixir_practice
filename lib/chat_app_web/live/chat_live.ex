2e63a322d8b1d623594877655b418c6b4bb0c636
Zokni1Coder <erikkovacs0000@gmail.com>
2024-10-28 19:30:09 +0000

    practice_12

--- lib/chat_app_web/live/chat_live.ex
+++ lib/chat_app_web/live/chat_live.ex
@@ -2,9 +2,13 @@ defmodule ChatAppWeb.ChatLive do
   use ChatAppWeb, :live_view
 
   alias Ecto.UUID
+  alias ChatAppWeb.Endpoint
+
+  @chat_topic "chat"
 
   def mount(_params, _session, socket) do
-    socket = socket |> stream(:messages, []) |> assign(form: to_form(%{}, as: "messages"))
+    if connected?(socket), do: Endpoint.subscribe(@chat_topic)
+    socket = socket |> stream(:messages, []) |> assign(form: to_form(%{}, as: "messages"), no_messages?: true)
     {:ok, socket}
   end
 
@@ -13,7 +17,17 @@ defmodule ChatAppWeb.ChatLive do
     {:noreply, socket}
   end
 
+  def handle_event("message", %{"messages" => %{"message" => ""}}, socket) , do: {:noreply, socket}
+
   def handle_event("message", %{"messages" => %{"message" => message}}, socket) do
+    socket = assign(socket, form: to_form(%{"message" => ""}, as: "messages"))
+
+    Endpoint.broadcast(@chat_topic, "message", message)
+    # IO.inspect(socket, label: "FUUUUCK")
+    {:noreply, socket}
+  end
+
+  def handle_info(%{topic: @chat_topic, event: "message", payload: message}, socket) do
     socket =
       socket
       |> stream_insert(:messages, %{
@@ -23,10 +37,7 @@ defmodule ChatAppWeb.ChatLive do
           DateTime.utc_now()
           |> Timex.Timezone.convert("Europe/Budapest")
           |> Timex.format!("%Y-%m-%d %H:%M:%S", :strftime)
-      })
-      |> assign(form: to_form(%{"message" => ""}, as: "messages"))
-
-    # IO.inspect(socket, label: "FUUUUCK")
-    {:noreply, socket}
+      }) |> assign(no_messages?: false)
+      {:noreply, socket}
   end
 end
