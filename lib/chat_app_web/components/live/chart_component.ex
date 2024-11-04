c2cc9a614fded8cbe9cb82f7472b76b96cfff540
Zokni1Coder <erikkovacs0000@gmail.com>
2024-11-04 19:39:25 +0000

    practice_17

--- lib/chat_app_web/components/live/chart_component.ex
+++ lib/chat_app_web/components/live/chart_component.ex
@@ -2,6 +2,7 @@ defmodule ChatAppWeb.Live.ChartComponent do
   use ChatAppWeb, :live_component
 
   @backgrounds ["bg-red-400", "bg-blue-400", "bg-green-400"]
+  @interval 2000
 
   def update(assigns, socket) do
     rnd_columns =
@@ -18,34 +19,35 @@ defmodule ChatAppWeb.Live.ChartComponent do
           ),
         else: %{}
 
+    assigns = Map.merge(assigns, rnd_columns)
+    send_update_after(__MODULE__, assigns, @interval)
     socket = assign(socket, assigns)
     {:ok, socket}
   end
 
-  def render(assigns) do
-    assigns = Map.put(assigns, :backgrounds, @backgrounds)
+  # assigns:
+  #   column_num:
+  #   rnd_columns: %{col1: %{value:22, color: blue @background}, col2: %{value:15, color: rand @background}, col3: %{value:25, color: rand @background}}
 
+  def render(assigns) do
     ~H"""
-    <div class="flex items-end h-52 w-full max-w-md gap-1">
-      <div
-        :for={column_index <- 1..@column_num}
-        style={"height: #{assigns[:"col#{column_index}"]}%;"}
-        class={"flex-1 #{Enum.random(@backgrounds)} rounded-t-xl transition-all"}
-      >
+    <div>
+      <span class="text-center block text-sm text-gray-600"><%= @content %></span>
+      <div class="flex items-end h-52 flex-1 gap-1 border-l-2 border-b-2 px-4 border-gray-400">
+        <%= for column_index <- 1..@column_num do %>
+          <% column_data = Map.get(assigns, :"col#{column_index}", %{}) %>
+          <% height_value = Map.get(column_data, :value) %>
+          <div
+            style={"height: #{height_value || 0}%;"}
+            class={"flex-1 #{Map.get(column_data, :color)} rounded-t-xl transition-all duration-1000 flex justify-center items-start py-1 min-w-10"}
+          >
+            <span class="bg-white rounded px-2 bg-opacity-50 text-gray-600 text-xs">
+              <%= height_value %>
+            </span>
+          </div>
+        <% end %>
       </div>
     </div>
     """
   end
-
-  defp rnd(true), do: Enum.random(0..100)
-
-  defp get_rnd_columns(column_num, connected? \\ true)
-
-  defp get_rnd_columns(column_num, connected?) when is_integer(column_num),
-    do:
-      Enum.reduce(1..column_num, [], fn column_index, column_assigns ->
-        column_assigns ++ [{:"col#{column_index}", rnd(connected?)}]
-      end)
-
-  defp get_rnd_columns(_column_num, _connected?), do: []
 end
